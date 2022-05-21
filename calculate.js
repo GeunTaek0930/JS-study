0996547dd0e90e5c4454e3ca4406cc225324b1d4
GeunTaek0930 <shawn09300@gmail.com>
2022-05-21 11:15:22 +0900

    2022-05-21 JS수의연산

--- calculate.js
+++ calculate.js
@@ -1,3 +1,11 @@
 alert(1 + 1);
 alert(1.2 + 1.0);
 alert(2 * 8);
+alert(6 / 2);
+console.log(6 / 2);
+console.log(Math.pow(3, 2)); //3의2승
+console.log(Math.round(10.6)); //반올림(정수)
+console.log(Math.ceil(10.2)); //올림(정수)
+console.log(Math.floor(10.2)); //내림(정수)
+console.log(Math.sqrt(9)); //제곱근
+console.log(Math.random()); //난수
