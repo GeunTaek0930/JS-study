8d876408b8c52c7d2db67cee7654acc7b80cc24a
GeunTaek0930 <shawn09300@gmail.com>
2022-05-22 16:04:20 +0900

    2022-05-22 js 문자

---
+++ text.js
@@ -0,0 +1,11 @@
+alert("coding everybody");
+alert('coding everybody');
+alert("egoing's coding everybody");
+alert('egoing"s coding everybody');
+alert('egoing\'s coding everybody');
+
+// typeof 1
+// "number"
+
+// typeof "1"
+// "string"
