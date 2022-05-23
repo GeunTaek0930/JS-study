32fbb2e9f22f9d82f52c4ffac3b99d7df305d19c
GeunTaek0930 <shawn09300@gmail.com>
2022-05-23 09:53:40 +0900

    2022-05-23 js 문자열

--- text.js
+++ text.js
@@ -9,3 +9,20 @@ alert('egoing\'s coding everybody');
 
 // typeof "1"
 // "string"
+
+alert("안녕하세요.\n코딩 세계에 오신 것을 환영합니다.");
+alert("coding" + " everybody");
+alert("coding\neverybody");  // \n 줄바꿈
+alert("coding"+" "+"everybody");
+
+console.log('1');
+console.log('"1"');
+console.log('1+1');
+console.log("1"+"1");
+console.log("coding everybody");
+console.log("coding everybody".length);
+console.log("code".length);
+console.log("code".indexOf("c"))
+console.log("code".indexOf("o"))
+console.log("code".indexOf("d"))
+console.log("code".indexOf("e"))
